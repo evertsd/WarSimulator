df5b4ea945ecf2db07cfe090b4a3b7e4113ccb39
Dosty Everts <dosty@mustwin.com>
2015-04-28 07:04:59 -0500

    #1 just about finished up a basic player class

--- player.js
+++ player.js
@@ -3,19 +3,41 @@ var Player = function() {
   var deck = [];
   var discard = [];
 
-  var setDeck = function(deck) {
-    this.deck = deck;
-    this.discard = [];
-  }
-
-  var stashWinnings = function(winnings) {
-    this.discard.concat(winnings);
-  }
-
-  var popCard = function() {
-    var popped = deck[deck.length - 1];
-    this.deck = deck.slice(0, deck.length - 1);
-
-    return popped;
-  }
+  return this;
 }
+
+Player.prototype.bottomOfDeck = function() {
+  return this.deck.length === 0 && this.discard.length > 0;
+}
+
+Player.prototype.isOut = function() {
+  return this.deck.length === 0 && this.discard.length === 0;
+}
+
+Player.prototype.popCard = function() {
+  var popped = null;
+  if(this.bottomOfDeck()) { this.makeStashDeck(); }
+  else if(this.isOut()) { return popped; }
+
+  popped = this.deck[this.deck.length - 1];
+  this.deck = this.deck.slice(0, this.deck.length - 1);
+
+  return popped;
+}
+
+Player.prototype.makeStashDeck = function() {
+  this.deck = this.discard;
+  this.discard = [];
+}
+
+Player.prototype.setDeck = function(deck) {
+  this.deck = deck;
+  this.discard = [];
+}
+
+Player.prototype.stashWinnings = function(winnings) {
+  this.discard.concat(winnings);
+}
+
+
+
