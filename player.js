df3b41c60e52da7ac9706b6e4afb8eeda08167a8
Dosty Everts <dosty@mustwin.com>
2015-04-29 00:38:00 -0500

    #2 started working on the war component, required a couple fixes with the player component as well. Almost have the basics down, still need to handle the 'war' case while trying to make this work for N players

--- player.js
+++ player.js
@@ -1,7 +1,7 @@
 var Player = function() {
 
-  var deck = [];
-  var discard = [];
+  this.deck = [];
+  this.discard = [];
 
   return this;
 }
@@ -14,14 +14,17 @@ Player.prototype.isOut = function() {
   return this.deck.length === 0 && this.discard.length === 0;
 }
 
-Player.prototype.popCard = function() {
+Player.prototype.popCard = function(number) {
   var popped = null;
-  if(this.bottomOfDeck()) { this.makeStashDeck(); }
-  else if(this.isOut()) { return popped; }
 
-  popped = this.deck[this.deck.length - 1];
-  this.deck = this.deck.slice(0, this.deck.length - 1);
+  while(number-- > 0) {
+    if(this.bottomOfDeck()) { this.makeStashDeck(); }
+    else if(this.isOut()) { return popped; }
 
+    popped = this.deck[this.deck.length - 1];
+    this.deck = this.deck.slice(0, this.deck.length - 1);
+  }
+  
   return popped;
 }
 
@@ -39,5 +42,23 @@ Player.prototype.stashWinnings = function(winnings) {
   this.discard.concat(winnings);
 }
 
+Player.prototype.addCardToDeck = function(card) {
+  this.deck.push(card);
+}
 
+Player.prototype.shuffleDeck = function() {
+  var deckClone = this.deck.slice(0),
+      newDeck = [],
+      deckCount = deckClone.length,
+      index, length;
 
+  while(newDeck.length < deckCount) {
+    index = parseInt(Math.random() * deckClone.length);
+    value = deckClone[index];
+    
+    deckClone = deckClone.slice(0, index).concat(deckClone.slice(index + 1, deckClone.length));
+    newDeck.push(value);
+  }
+
+  this.deck = newDeck;
+}
--- war.js
+++ war.js
@@ -1,23 +1,47 @@
-var leftSide = [];
-var rightSide = [];
-var fullSide = [2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14];
+var War = function(numberOfPlayers) {
 
-var fillSide = function(array) {
-	var fullClone = fullSide.slice(0),
-		  tempArray = [],
-      fullCount = fullClone.length,
-      index, value;
+  this.players = [new Player(), new Player()];
+  this.fullDeck = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4,
+                  5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7,
+                  8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10,
+                  11, 11, 11, 11, 12, 12, 12, 12, 13, 13, 13, 13, 14, 14, 14, 14];
 
-  while(tempArray.length < fullCount) {
-    index = parseInt(Math.random() * fullClone.length);
-    value = fullClone[index];
-    fullClone = fullClone.slice(0, index).concat(fullClone.slice(index + 1, fullClone.length));
-    tempArray.push(value);
+  this.initPlayers();
+}
+
+War.prototype.initPlayers = function() {
+  var index = 0;
+
+  while(index < this.fullDeck.length) {
+    this.players[0].addCardToDeck(this.fullDeck[index++])
+    this.players[1].addCardToDeck(this.fullDeck[index++])
   }
 
-  console.log(tempArray);
-  array = tempArray;
-  return tempArray;
+  this.players[0].shuffleDeck();
+  this.players[1].shuffleDeck();
+}
+
+War.prototype.topCards = function(number) {
+  var cards = [];
+  number = number || 1;
+
+  for(var i = 0; i < this.players.length; i++) {
+    cards[i] = this.players[i].popCard(number);
+  }
+
+  return cards;
+}
+
+War.prototype.findWinner = function() {
+  var cards = this.topCards(),
+      winners = [];
+
+  for(var i = 0; i < this.players.length; i++) {
+
+  }
+}
+War.prototype.battle = function() {
+
 }
 
 var avgDifference = function(diffArray) {
